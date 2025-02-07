e7a31ace62c95d06110f19fe94a39fb01302bc6f
aneesurrahiman.s <aneesurrahiman.s@nagra.com>
2025-02-07 19:20:13 +0000

    updated code

--- gameCode.js
+++ gameCode.js
@@ -1,226 +1,130 @@
+class TicTacToe {
+    static SIZE = 300;
+    static MIN_BUTTONS = 3;
+    static MAX_BUTTONS = 10;
 
-var ticTacToebtnElems = function() {
-	this.div='td';
-	this.class='btn btn-default';
-	this.text='+';
-	this.isClicked=false;
-	this.whoClicked='';
-	this.position;
+    constructor(size) {
+        this.size = this.validateSize(size);
+        this.count = 0;
+        this.btns = [];
+        this.x_win = 0;
+        this.o_win = 0;
+        this.appendPlace = document.getElementById('tttBtns');
+        this.createNewBtns();
+		this.statusElem = document.getElementById('status');
+		this.o_win_id = document.getElementById('o_win');
+		this.x_win_id = document.getElementById('x_win');
+    }
+
+    validateSize(size) {
+        if (typeof size !== 'number' || size < TicTacToe.MIN_BUTTONS || size > TicTacToe.MAX_BUTTONS) {
+            throw new Error('Size must be a number between 3 and 10');
+        }
+        return size;
+    }
+
+    createNewBtns() {
+        for (let i = 0; i < this.size; i++) {
+            this.btns[i] = [];
+            const tr = document.createElement('tr');
+            this.appendPlace.appendChild(tr);
+            for (let j = 0; j < this.size; j++) {
+                const btnElem = new TicTacToeBtnElem(this, i, j);
+                btnElem.createBtn(tr);
+                this.btns[i][j] = btnElem;
+            }
+        }
+    }
+
+    reset() {
+        this.count = 0;
+        this.clearBoard();
+        this.createNewBtns();
+    }
+
+    clearBoard() {
+        while (this.appendPlace.rows.length) {
+            this.appendPlace.deleteRow(0);
+        }
+    }
+
+    checkWinCondition(obj, text) {
+        const [row, col] = obj.position.split('.').map(Number);
+
+        if (this.count === this.size * this.size) return 'tie';
+
+        const checkLine = (line) => line.every(cell => cell.whoClicked === text);
+
+        const rowWin = checkLine(this.btns[row]);
+        const colWin = checkLine(this.btns.map(r => r[col]));
+        const diag1Win = row === col && checkLine(this.btns.map((r, i) => r[i]));
+        const diag2Win = row + col === this.size - 1 && checkLine(this.btns.map((r, i) => r[this.size - 1 - i]));
+
+        if (rowWin || colWin || diag1Win || diag2Win) return text;
+    }
+
+    updateStatus(message) {
+		this.statusElem.textContent = message;
+		
+		this.o_win_id.value = this.o_win;
+		this.x_win_id.value = this.x_win;
+
+        // if (statusElem) {
+        //     statusElem.textContent = message;
+        // }
+    }
 }
 
-ticTacToebtnElems.prototype.createBtn = function(tr,i,j) {
-	this.btn = document.createElement(this.div);
-	this.btn.className = this.class;
-	this.btn.textContent = this.text;
-	this.position = i +'.'+j;
-	this.btn.style.height = size/noofBtns+'px';
-	this.btn.style.width = size/noofBtns+'px';
-	this.btn.style.lineHeight = size/noofBtns+'px';
-	this.btn.style.padding = '0px';
-	this.btn.style.margin = '2px';
-	this.btn.style.fontSize = size/(noofBtns*3)+'px';
-		 tr.appendChild(this.btn);
-	this.click = this.btn.addEventListener('click', clickFunc.bind(this));
-};
+class TicTacToeBtnElem {
+    constructor(game, i, j) {
+        this.game = game;
+        this.div = 'td';
+        this.class = 'btn btn-default';
+        this.text = '+';
+        this.isClicked = false;
+        this.whoClicked = '';
+        this.position = `${i}.${j}`;
+    }
 
-var clickFunc = function() {
+    createBtn(tr) {
+        this.btn = document.createElement(this.div);
+        this.btn.className = this.class;
+        this.btn.textContent = this.text;
+        this.btn.style.height = TicTacToe.SIZE / this.game.size + 'px';
+        this.btn.style.width = TicTacToe.SIZE / this.game.size + 'px';
+        this.btn.style.lineHeight = TicTacToe.SIZE / this.game.size + 'px';
+        this.btn.style.padding = '0px';
+        this.btn.style.margin = '2px';
+        this.btn.style.fontSize = TicTacToe.SIZE / (this.game.size * 3) + 'px';
+        tr.appendChild(this.btn);
+        this.btn.addEventListener('click', this.clickFunc.bind(this));
+    }
 
-	if(this.isClicked)
-	{
-		alert('already selected');
-	}
-	else if(!this.isClicked)
-	{
-		this.isClicked = true;
-		count % 2 > 0 ? this.text = 'X' : this.text = 'O';
-		this.btn.textContent = this.text;
-		this.whoClicked = this.text;
+    clickFunc() {
+        if (this.isClicked) {
+            this.game.updateStatus('Already selected');
+            return;
+        }
+		this.game.statusElem.textContent = '';
+        this.isClicked = true;
+        this.text = this.game.count % 2 === 0 ? 'X' : 'O';
+        this.btn.textContent = this.text;
+        this.whoClicked = this.text;
+        this.btn.style.backgroundColor = this.text === 'X' ? '#c9f9f9' : '#55a0aa';
 
-		if(this.text == 'X')
-		{
-			this.btn.style.backgroundColor = '#c9f9f9';
-		}
-		else if(this.text == 'O')
-		{
-			this.btn.style.backgroundColor = '#55a0aa';
-		}
-
-		count++;
-		var whoWon = winCondition(this,this.text);
-
-		if(whoWon == 1)
-		{
-			count = 0;
-			deleteRow();
-			createNewBtns();
-			alert('It is a tie');
-		}
-		else if(whoWon == 'X')
-		{
-			x_win_id.value = ++x_win;
-			btns.length = 0;
-			count = 0;
-			deleteRow();
-			createNewBtns();
-			alert('X won the game');
-		}
-		else if(whoWon == 'O')
-		{
-			o_win_id.value = ++o_win;
-			btns.length = 0;
-			count = 0;
-			deleteRow();
-			createNewBtns();
-			alert('O won the game');
-		}
-	}
-};
-
-var winCondition = function(obj,text) {
-
-	var res = obj.position.split('.');
-	var player;
-	text == 'O'?player = 'X':player = 'O'
-
-	if(count == noofBtns*noofBtns)
-	{
-		return 1;
-	}
-
-	for(var i = 0; i < noofBtns; i++)
-	{
-		if(btns[res[0]][i].whoClicked == text)
-		{
-			arrHorWin.push(btns[res[0]][i].text);
-		}
-	}
-
-	for(var i = 0; i < noofBtns; i++)
-	{
-		if(btns[i][res[1]].whoClicked == text)
-		{
-			arrVerWin.push(btns[i][res[1]].text);
-		}
-	}
-
-	if((res[0] == 0) && (res[1] == 0) || 
-	   (res[0] == 1) && (res[1] == 1) ||
-	   (res[0] == 2) && (res[1] == 2) ||
-	   (res[0] == 3) && (res[1] == 3) ||
-	   (res[0] == 4) && (res[1] == 4) ||
-	   (res[0] == 5) && (res[1] == 5) ||
-	   (res[0] == 6) && (res[1] == 6) ||
-	   (res[0] == 7) && (res[1] == 7) ||
-	   (res[0] == 8) && (res[1] == 8) ||
-	   (res[0] == 9) && (res[1] == 9) )
-	{
-	   	if(arrDiagWin.length == 0)
-	   	{
-			for (var i = 0; i < noofBtns; i++)
-			{
-				if(btns[i][i].whoClicked == text)
-				{
-					arrDiagWin.push(btns[i][i].text)
-				}
-			}
-		}
-	}
-	if((res[0] == 0) && (res[1] == noofBtns-1) ||
-	   (res[0] == 1) && (res[1] == noofBtns-2) ||
-	   (res[0] == 2) && (res[1] == noofBtns-3) ||
-	   (res[0] == 3) && (res[1] == noofBtns-4) ||
-	   (res[0] == 4) && (res[1] == noofBtns-5) ||
-	   (res[0] == 5) && (res[1] == noofBtns-6) ||
-	   (res[0] == 6) && (res[1] == noofBtns-7) ||
-	   (res[0] == 7) && (res[1] == noofBtns-8) ||
-	   (res[0] == 8) && (res[1] == noofBtns-9) ||
-	   (res[0] == 9) && (res[1] == noofBtns-10) )
-	{
-	   	if(arrDiagWin.length == 0)
-	   	{
-			for (var i = 0; i < noofBtns; i++)
-			{
-				if(btns[i][noofBtns-1-i].whoClicked == text)
-				{
-					arrDiagWin.push(btns[i][noofBtns-1-i].text)
-				}
-			}
-		}
-	}
-
-	if(arrHorWin.length == noofBtns)
-	{
-		if(arrHorWin.indexOf(player) < 0)
-		{
-			return text;
-		}
-	}
-	else
-	{
-		arrHorWin = [];
-	}
-
-	if(arrVerWin.length == noofBtns)
-	{
-		if(arrVerWin.indexOf(player) < 0)
-		{
-			return text;
-		}
-	}
-	else
-	{
-		arrVerWin = [];
-	}
-
-	if(arrDiagWin.length == noofBtns)
-	{
-		if(arrDiagWin.indexOf(player) < 0)
-		{
-			return text;
-		}
-	}
-	else
-	{
-		arrDiagWin = [];
-	}
+        this.game.count++;
+        const result = this.game.checkWinCondition(this, this.text);
+		console.log('result', result);
+        if (result === 'tie') {
+            this.game.updateStatus('It is a tie');
+            this.game.reset();
+        } else if (result) {
+			result === 'X' ? this.game.x_win++ : this.game.o_win++;
+            this.game.updateStatus(`${result} won the game`);
+            // this.game[result === 'X' ? 'x_win' : 'o_win']++;
+			console.log('this.game.x_win', this.game.x_win);
+			console.log('this.game.o_win', this.game.o_win);
+            this.game.reset();
+        }
+    }
 }
-
-var deleteRow = function(){
-	arrHorWin = [];
-	arrVerWin = [];
-	arrDiagWin = [];
-	while(appendPlace.rows.length)
-	{
-		appendPlace.deleteRow(0);	
-	}
-}
-
-var createNewBtns = function(){
-	if(noofBtns < 3 || noofBtns > 10)
-	{
-		alert('please enter in range of 3 to 10');
-		window.location.reload();
-	}
-	else
-	{
-		for (var i = 0; i<noofBtns; i++)
-		{
-			btns[i] = [];
-			var tr = document.createElement('tr');
-			appendPlace.appendChild(tr);
-				
-			for(var j=0; j<noofBtns; j++)
-			{
-				btns[i][j] = new ticTacToebtnElems();
-				btns[i][j].createBtn(tr,i,j);
-			}
-		}
-	}
-}
-
-var reset = function(){
-	count = 0;
-	deleteRow();
-	createNewBtns();
-}
-//443743932339
--- gameStarter.js
+++ gameStarter.js
@@ -1,20 +1,3 @@
-var appendPlace = document.getElementById('tttBtns');
-var btns = [];
-var count = 0;
-var noofBtns;
-var size = 400;
-var sum = 1;
-var o_win = 0;
-var x_win = 0;
-var o_win_id = document.getElementById('o_win');
-var x_win_id = document.getElementById('x_win');
-var arrHorWin = [];
-var arrVerWin = [];
-var arrDiagWin = [];
-var resetBtn = document.getElementById('reset');
-resetBtn.addEventListener('click',reset);
-
-
 var noofBtns = prompt("Please enter number of rows required");
 
 if (noofBtns == null || noofBtns == "") 
@@ -23,7 +6,11 @@ if (noofBtns == null || noofBtns == "")
 } 
 else 
 {
-	createNewBtns();
+	try {
+        const game = new TicTacToe(Number(noofBtns)); 
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 
